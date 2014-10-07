b0634dcf784777fd8f39e82934e0c3a5e914a58d
Alex <alex.nimmons@gmail.com>
2014-10-07 16:30:27 -0400

    main jquery complete

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-jquery(document).ready(fucntion() {
-	
-}
+$(document).ready(function(){
+
+	var addItem= function(){
+		var input = $("#myInput").val() 
+		$("#list").append("<li class='item'>"+input+"</li>");
+			$("#myInput").val(""); 
+	}
+
+	$("#myButton").click(addItem);
+	$(document).keydown(function(k) {
+		if (k.keyCode == 13){
+			addItem();
+		}
+	});
+
+$("ol").on('click', '.item', function(){
+	$(this).toggleClass("checked-off");
+})
+
+
+$('ol').on('dblclick', '.item', function(){
+	$(this).remove();
+})
+
+})
